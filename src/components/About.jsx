b9f645ad4e82d7ab857ee9caef194a93439443e1
SwatejReddy <swatejreddy17@gmail.com>
2024-07-20 15:04:19 +0530

    Initial commit of my project

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+              About
+            </p>
+          </div>
+          <div></div>
+          </div>
+          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+            <div className='sm:text-right text-4xl font-bold'>
+              <p>Hi. I'm Swatej Reddy, nice to meet you. Please take a look around.</p>
+            </div>
+            <div>
+              <p>I am a backend developer with a slight bit of interest in the frontend. I absolutely love working on backend technologies but don't really enjoy working on frontend part until it is necessary to do so. Although I do like a change sometimes.</p>  
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/data/data.js
@@ -0,0 +1,39 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+import Askout from '../assets/projects/askout.jpg'
+import Medium from '../assets/projects/medium.jpg'
+import Rps from '../assets/projects/rps.jpg'
+import EtchASketch from '../assets/projects/etch-a-sketch.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"Fullstack - Blogging website",
+        image:Medium,
+        github:"https://github.com/SwatejReddy/Medium",
+        live:"",
+    },
+    {
+        id:2,
+        name:"AskOut",
+        image:Askout,
+        github:"https://github.com/SwatejReddy/surprise",
+        live:"https://surprise-plum.vercel.app/",
+    },
+    {
+        id:3,
+        name:"Etch A Sketch",
+        image:EtchASketch,
+        github:"https://github.com/SwatejReddy/etch-a-sketch",
+        live:"https://etch-a-sketch-xi-seven.vercel.app/",
+    },
+    {
+        id:4,
+        name:"Rock Paper Scissors",
+        image:Rps,
+        github:"https://github.com/SwatejReddy/rock-paper-scissors",
+        live:"https://rock-paper-scissors-two-jet.vercel.app/",
+    },
+
+
+]
