78056be71deb665f940462778489adb1c74f8937
Swatej Reddy <109172700+SwatejReddy@users.noreply.github.com>
2024-10-02 13:03:43 +0530

    Update Navbar.jsx
    Update resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -125,7 +125,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/157N8CmoTqzCRiCn1XoFtTyrGtblLnz-K/view?usp=sharing' target='_blank'
+              href='https://drive.google.com/file/d/1w72VU5Eb2qg_YsVuun79wD3larCXlttR/view?usp=sharing' target='_blank'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
