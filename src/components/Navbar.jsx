7236001bd805073e98004bc25b24c5caa74c2106
Rajkumar <anumularajkumar2003@gmail.com>
2024-10-26 13:57:33 +0530

    zap updated drive new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -118,7 +118,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/1_LAsiN2NTatKwUuVV4S27u_ZtMVwKL0a/view?usp=drivesdk' target='_blank'
+              href='https://drive.google.com/file/d/1_PkhJe4KgaIykNGxlY2W27aE9oM8d2pO/view?usp=drivesdk' target='_blank'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
