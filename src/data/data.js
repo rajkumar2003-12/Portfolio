6d44691b125015f78fa7adebf35ebc2585e53545
Rajkumar <anumularajkumar2003@gmail.com>
2024-10-26 13:14:16 +0530

    upload pic

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,6 @@
 import WorkIm from '../assets/projects/workImg.jpeg'
 import RealEst from '../assets/projects/realestate.jpg'
 import Askout from '../assets/projects/askout.jpg'
-import Medium from '../assets/projects/medium.jpg'
 import Rps from '../assets/projects/rps.jpg'
 import EtchASketch from '../assets/projects/etch-a-sketch.jpg'
 
@@ -9,7 +8,7 @@ export const data=[
     {
         id:1,
         name:"Fullstack - Zap Web Application",
-        image:Medium,
+        image:"C:\Users\anumu\OneDrive\Desktop\FullStackProjects\Portfolio\portfolio-new-1\src\assets\Zap entry.png",
         github:"https://github.com/rajkumar2003-12",
         live:"https://zap-rajkumar.vercel.app/",
     },
