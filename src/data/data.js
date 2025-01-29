8af5fb23865679e6cf02659bff0a7599a41db520
Rajkumar <anumularajkumar2003@gmail.com>
2025-01-29 23:05:01 +0530

    commit

--- src/data/data.js
+++ src/data/data.js
@@ -1,12 +1,33 @@
 
 import zap from '../assets/zappage.png'
+import matrimony from "../assets/matrimony.mp4"
 
-export const data=[
+export const data1=[
     {
         id:1,
         name:"Fullstack - Zap Web Application",
         image:zap,
         github:"https://github.com/rajkumar2003-12",
         live:"https://zap-rajkumar.vercel.app/",
+    },
+]
+
+export const data2=[
+    {
+        id:1,
+        name:"Fullstack - Matrimony web application",
+        image:matrimony,
+        github:"https://github.com/rajkumar2003-12",
+        live:"https://saptjanm-matrimony.vercel.app/",
+    },
+]
+
+export const data3=[
+    {
+        id:1,
+        name:"Fullstack - Matrimony web application",
+        image:matrimony,
+        github:"https://github.com/rajkumar2003-12",
+        live:"https://saptjanm-matrimony.vercel.app/",
     },
 ]
